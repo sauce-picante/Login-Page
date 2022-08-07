5a85ade33fff561a4532cd3c55a2dc2c4d808370
sauce-picante <tdougherty16@gmail.com>
2022-08-07 17:18:46 -0400

    refactoring, added password and email validation

--- src/main.js
+++ src/main.js
@@ -4,8 +4,7 @@
  * 
  * ****************************************************************
  */
-
-function setFormMessage(formElement, messageType, message){
+ function setFormMessage(formElement, messageType, message){
     const messageElement = formElement.querySelector(".form--message");
 
     messageElement.textContent = message;
@@ -32,35 +31,57 @@ function clearInputError(inputElement){
     inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
 }
 function clearInputSuccess(inputElement){
-    inputElement.classList.remove("form_input-success");
+    inputElement.classList.remove("form_input--success");
     inputElement.parentElement.querySelector(".form__input-success-message").textContent = "";
 }
-/*
+/**********************************************************************
+ *              Input Validation  
+ * 
  * Function for username and password character validation
  * username: No special characters allowed
  * 
  * the regex [^a-zA-Z0-9] should match a single character 
  * that is not alphanumeric 
  * 
- * password: must contain at least one upper and lower case letter, 
+ * password regex will look for 8 characters, at least one upper and lower case letter, 
  * at least one special character and at aleast one number.
+ * 
+ * TO DO: Fix Valid Username prompt upon mouse clicking the field
  */
 function invalidCharacters(inputText){
-    var specialChar = /[^a-zA-Z0-9]/g;
-    var username = inputText;
-    return (specialChar.test(username));
+    const invalidRegex = /[^a-zA-Z0-9]/g;
+    return (invalidRegex.test(inputText));
 }
 
 function usernameLength(inputText){
-    const username = inputText;
-    return (username.length > 0 && username.length < 4);
+    return (inputText.length > 0 && inputText.length < 4);
 }
 
 function validPassword(inputText){
-    const password = inputText;
-    const pattern = /^(?=.{8,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?\W).*$/g;
-    return (pattern.test(inputText));
+    const passwordRegex = /^(?=.{8,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?\W).*$/g;
+    return (passwordRegex.test(inputText));
 }
+
+function comparePasswords(inputText, password){
+    if(typeof inputText === 'undefined' || inputText === null || inputText === ""){
+        return false;
+    }
+    if(typeof password === 'undefined' || password === null || password === ""){
+        return false;
+    }
+    return (inputText == password);
+}
+
+//  NOTE: Testing for a valid email address through a regular expression
+//  is not good practice. This could potentially filter out unconventional
+//  but perfectly valid email addresses.
+//
+//  TO DO: Find a fancier way to validate email
+function validEmail(inputText){
+    const emailRegex= / \S+@\S+\.\S+/;
+    return emailRegex.test(inputText);
+}
+
 /* DOMContentLoaded event is triggered when initial HTML document has been
 *  loaded and parsed. the " document " interface represents the HTML document
 *  and allows modification to that HTML document
@@ -78,13 +99,13 @@ document.addEventListener("DOMContentLoaded", () => {
     //that matches the parameter
     document.querySelector("#linkCreateAccount").addEventListener("click", event => {
         event.preventDefault();
+        
         //classList property returns the CSS classnames of an element as a DOMTokenList
         //can be accessed by index
         loginForm.classList.add("form--hidden");
         forgotPassword.classList.add("form--hidden");
         createAccountForm.classList.remove("form--hidden");
-       
-    });
+     });
 
     document.querySelector("#linkLogin").addEventListener("click", event => {
         event.preventDefault();
@@ -107,14 +128,68 @@ document.addEventListener("DOMContentLoaded", () => {
         forgotPassword.classList.add("form--hidden");
         createAccountForm.classList.add("form--hidden");
     });
+    
+    /************************************************
+     ***** Account Creation form input
+     ***********************************************
+     */
+    document.querySelectorAll(".form__input").forEach(inputElement => {
+        inputElement.addEventListener("blur", event => {
+            event.preventDefault();
+            
+            // Username 
+              if(event.target.id === "signupUsername" && event.target.value.length > 0 && event.target.value.length < 4) {
+                    setInputError(inputElement, "Username must be at least 4 characters long.");
+                
+             } else if(event.target.id === "signupUsername" && invalidCharacters(event.target.value) === true){
+                    setInputError(inputElement, "Username may contain alphanumeric characters only.");
+               
+             } else if (event.target.id === "signupUsername" && invalidCharacters(event.target.value) === false) {
+                    setInputSuccess(inputElement, "Username is valid!");
+              }
+            // Password
+              if(event.target.id === "signupPassword" && validPassword(event.target.value) === true){
+                setInputSuccess(inputElement, "Password is valid!");
+                }else if(event.target.id === "signupPassword" && validPassword(event.target.value) === false) {
+                    setInputError(inputElement, "Password must: Be at least 8 characters long. "
+                                                + "Contain at least one number, one special character,"
+                                                + " an upper and a lower chase letter");
+                                        
+            }
+            
+            // Confirm Password
+            let password = document.getElementById("signupPassword").value;
+            if(event.target.id === "confirmPassword" && validPassword(password) && comparePasswords(event.target.value, password)){
+                 setInputSuccess(inputElement, "Passwords match!");
+            } else if(event.target.id === "confirmPassword" && comparePasswords(event.target.value, password) === false){
+                setInputError(inputElement, "Passwords do not match");
+                
+            }
+            
+          //Enter email
+            if(event.target.id === "signupEmail"){
+                if(validEmail(event.target.value) === true){
+                    setInputSuccess(inputElement, "Email address is valid");
+                } else {
+                    setInputError(inputElement, "Please enter a valid email address");
+                }
+            }
+        });
+        // Clear messages when input is detected
+        inputElement.addEventListener("keypress", event => {
+                clearInputSuccess(inputElement);
+                clearInputError(inputElement);
+                
+        });
+     });
 
-    //Login form submission
+    /**************************************************
+     *         Form Submission Events
+     **************************************************/
+     //Login form submission
     loginForm.addEventListener("click", event => {
-        
         event.preventDefault();
-        
         //login stuff here like AJAX server communication
-
         setFormMessage(loginForm, "error", "Invalid username or password");
     });
 
@@ -127,35 +202,4 @@ document.addEventListener("DOMContentLoaded", () => {
     forgotPassword.addEventListener("submit", event => {
         event.preventDefault();
     });
-    /************************************************
-     ***** Account Creation form input
-     ************************************************/
-
-    
-        document.querySelectorAll(".form__input").forEach(inputElement => {
-         inputElement.addEventListener("blur", event => {
-            event.preventDefault();
-              if(event.target.id === "signupUsername" && event.target.value.length > 0 && event.target.value.length < 4) {
-                    setInputError(inputElement, "Username must be at least 4 characters long.");
-                
-             } else if(event.target.id === "signupUsername" && invalidCharacters(event.target.value) === true){
-                    setInputError(inputElement, "Username may contain alphanumeric characters only.");
-               
-             } else if (event.target.id === "signupUsername" && invalidCharacters(event.target.value) === false) {
-                    setInputSuccess(inputElement, "Username is valid!");
-              }
-
-              if(event.target.id === "signupPassword" && validPassword(event.target.value) === false){
-                    setInputError(inputElement, "Password must: Be at least 8 characters long. Contain at least one number, one special character, an upper and a lower chase letter");
-              } else if(event.target.id === "signupPassword" && validPassword(event.target.value) === true){
-                    setInputSuccess(inputElement, "Password is valid!");
-              }
-            });
-            inputElement.addEventListener("input", event => {
-                    clearInputError(inputElement);
-                    clearInputSuccess(inputElement);
-            })
-     });
-    
-    
 });
